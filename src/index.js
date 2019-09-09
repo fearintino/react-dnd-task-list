16692e05a903bc4db7aff84dab059f6e138df5ac
Мухтаров Тимур <tnmukhtarov@vostbank.ru>
2019-09-09 15:33:26 +0300

    Delete boilerplate

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 import configureStore from './modules/store';
+
+import App from './App';
 
 const reduxStore = configureStore();
 
@@ -24,8 +23,3 @@ if (module.hot) {
     render();
   });
 }
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
