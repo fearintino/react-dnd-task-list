f645c35860ea7cfa1dbaa86ef8e4ee9a36361fe7
Мухтаров Тимур <tnmukhtarov@vostbank.ru>
2019-09-10 15:00:02 +0300

    Updates

---
+++ precache-manifest.43bb8c95235bdf5c83349dc46e747b82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90c15601ee1df13bb491db7a38a3974",
+    "url": "/react-dnd-task-list/index.html"
+  },
+  {
+    "revision": "50a9fdef1c5a32df092f",
+    "url": "/react-dnd-task-list/static/css/2.98b27591.chunk.css"
+  },
+  {
+    "revision": "50a9fdef1c5a32df092f",
+    "url": "/react-dnd-task-list/static/js/2.f69751ad.chunk.js"
+  },
+  {
+    "revision": "402c06e7a0bfefaf9898",
+    "url": "/react-dnd-task-list/static/js/main.d1ec78bb.chunk.js"
+  },
+  {
+    "revision": "2f3a22394b27a2182293",
+    "url": "/react-dnd-task-list/static/js/runtime~main.385a7756.js"
+  }
+]);
